b4d5b6550675d2144173ac7ab7778e3c3477b726
Acer <Acer@DESKTOP-ABKHEEV>
2024-12-08 17:09:07 +0530

    Add project files.

---
+++ NimapDEMO/Controllers/CategoryController.cs
@@ -0,0 +1,48 @@
+﻿using NimapDEMO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList.Mvc;
+using PagedList;
+
+namespace NimapDEMO.Controllers
+{
+    
+    public class CategoryController : Controller
+    {
+        NimapDBEntities1 dbo=new NimapDBEntities1();
+        // GET: Category
+        public ActionResult Index(int?i)
+        {
+            var list = dbo.sp_join().ToList().ToPagedList(i??1,5);
+           
+            return View(list);
+        }
+        [HttpGet]
+        public ActionResult IndexSearch(string txtSearch, int? i)
+        {
+            //var list = dbo.sp_join().ToList().ToPagedList(i ?? 1, 5);          
+            var Tlist = dbo.sp_join().ToList();
+            var list = Tlist.Where(x => x.CategoryName.ToLower().Contains(txtSearch)).ToList().ToPagedList(i ?? 1, 5);
+            return View("Index",list);
+        }
+        [HttpGet]
+        public ActionResult Search(string txtSearch, int? i)
+        {
+           // var categories = dbo.tblCategories.ToList().ToPagedList(i??1,2);
+            var categories = dbo.tblCategories.Where(x => x.CategoryName.Contains(txtSearch)||txtSearch==null).ToList().ToPagedList(i??1, 5);
+
+            return View(categories);
+        }
+        [HttpPost]
+        public ActionResult Search(string txtSearch)
+        {
+           var categories = dbo.tblCategories.Where(x=>x.CategoryName.Contains(txtSearch)).ToList().ToPagedList(1,5);
+           
+            return View(categories);
+
+        }
+    }
+}
---
+++ NimapDEMO/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NimapDEMO.MvcApplication" Language="C#" %>
